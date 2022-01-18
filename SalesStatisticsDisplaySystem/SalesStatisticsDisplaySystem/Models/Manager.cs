a5e75b6e3c8d2c848088ddf6104667075e22e799
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-18 20:12:10 +0300

    Create Models fodler and few models

---
+++ SalesStatisticsDisplaySystem/SalesStatisticsDisplaySystem/Models/Manager.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesStatisticsDisplaySystem.Models
+{
+    public class Manager
+    {
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ SalesStatisticsDisplaySystem/SalesStatisticsDisplaySystem/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesStatisticsDisplaySystem.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime Date { get; set; }
+
+        public decimal Sum { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public Manager Manager { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
