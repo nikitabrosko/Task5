27ad78678fd0c942032825119dd1131d49607dc7
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-24 21:44:01 +0300

    Change file orderAjax.js

--- SalesStatisticsDisplaySystem/SalesStatisticsDisplaySystem/wwwroot/js/orderAjax.js
+++ SalesStatisticsDisplaySystem/SalesStatisticsDisplaySystem/wwwroot/js/orderAjax.js
@@ -15,7 +15,7 @@ function fetchOrderTable() {
             $("tbody").empty();
             for (let i = 0; i < data.length; i++) {
                 $("tbody").append(
-                    `<tr><td>${data[i].id}</td><td>${data[i].date}</td><td>${data[i].sum}</td><td>${data[i].customer.Id}</td><td>${data[i].manager.Id}</td><td>${data[i].product.Id}</td></tr>`);
+                    `<tr><td>${data[i].id}</td><td>${data[i].date}</td><td>${data[i].sum}</td><td>${data[i].customerFullName}</td><td>${data[i].managerLastName}</td><td>${data[i].productName}</td></tr>`);
             }
         },
         error: function (error) {
