d696859ebb60cd9a03e5a02530f25c01e70ae7ad
Nik.Brosko <Nikita.brosko@mail.ru>
2022-01-24 21:43:22 +0300

    Refactoring Customer and Order classes

--- SalesStatisticsDisplaySystem/SalesStatisticsDisplaySystem/Models/Order.cs
+++ SalesStatisticsDisplaySystem/SalesStatisticsDisplaySystem/Models/Order.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesStatisticsDisplaySystem.Models
 {
@@ -7,15 +7,16 @@ namespace SalesStatisticsDisplaySystem.Models
     {
         public int Id { get; set; }
 
-        [Column(TypeName = "Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd.MM.yyyy}")]
         public DateTime Date { get; set; }
 
         public decimal Sum { get; set; }
 
-        public Customer Customer { get; set; }
+        public string CustomerFullName { get; set; }
 
-        public Manager Manager { get; set; }
+        public string ManagerLastName { get; set; }
 
-        public Product Product { get; set; }
+        public string ProductName { get; set; }
     }
 }
